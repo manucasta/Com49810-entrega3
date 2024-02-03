a9caaa2dd5154ba8054fd58b5db5650095ae7e2f
manu.casta <manuel.castanedaok@gmail.com>
2024-02-03 14:46:28 -0500

    final proyect

--- index.js
+++ index.js
@@ -137,9 +137,137 @@ function removeCart(idProducto){
 
 createCard ();
 
+const urlPosiciones = "https://v3.football.api-sports.io/standings?league=128&season=2024";
+const urlFixture = "https://v3.football.api-sports.io/fixtures?league=128&season=2024&team=453"
+const container = document.getElementById("futbol");
+
+fetch(urlFixture, {
+	"method": "GET",
+	"headers": {
+		"x-rapidapi-host": "v3.football.api-sports.io",
+		"x-rapidapi-key": "b87cccf3972083898b10f66abbd3b5b5"
+	}
+})
+.then(response => response.json())
+.then((result)=>{
+	console.log(result.response);
+	mostrarFixture(result.response);
+})
+.catch(err => {
+	console.log(err);
+});
+
+function mostrarFixture (fixture){
+	const league = fixture[0].league.name;
+
+	container.querySelector(".fixture").innerHTML +=`
+	<h3 class="title-next-match">FIXTURE</h3>
+	<h4>${league}</h4>
+	<div class="fixture-scroll">
+	`
+
+	fixture.forEach(fechas=>{
+		const fecha = fechas.league.round;
+		const teams = fechas.teams;
+		const date = fechas.fixture.date;
+		const stadium = fechas.fixture.venue;
+		const referee = fechas.fixture.referee;
+
+		container.querySelector(".fixture .fixture-scroll").innerHTML += `
+      <div class="fecha-box"
+        <h4> ${fecha} </h4>
+        <div class="teams-box">
+          <div class="local-team">
+            <div>
+            <img src="${teams.home.logo}" class="team-logo" alt="equipo local"> ${teams.home.name}
+            </div>
+          </div>
+          <div class="versus">
+            <div>X</div>
+          </div>
+          <div class="away-team" style= "margin-bottom: 10px;">
+            <div>
+            <img src="${teams.away.logo}" class="team-logo" alt="equipo visitante"> ${teams.away.name}
+            </div>
+          </div>
+        </div>
+        <div class="match-details">
+          <div>${date}</div>
+          <div>${stadium.name}</div>
+          <div>${stadium.city}</div>
+          <div>${referee}</div>
+        </div>
+      </div>
+		`
+		container.querySelector(".fixture").innerHTML +=`
+			</div>
+		</div>
+		`;
+	})
+}
+
+fetch(urlPosiciones, {
+  method: "GET",
+  headers: {
+    "x-rapidapi-host": "v3.football.api-sports.io",
+    "x-rapidapi-key": "b87cccf3972083898b10f66abbd3b5b5",
+  },
+})
+  .then((response) => response.json())
+  .then((data) => {
+		console.log (data);
+		const league = data.response[0].league;
+		const standings = data.response[0].league.standings[0];
+		listarEquipos(league, standings);
+  })
+  .catch((err) => console.log(err));
+
+function listarEquipos(league, standings) {
+
+	container.querySelector(".positions").innerHTML += `
+	<h3 class="title-next-match">POSICIONES</h4>
+	<h4>${league.name}</h2>
+	`;
+
+  container.querySelector(".positions").innerHTML += `
+	<div class="scrollable-table">
+    <table class="table-positions">
+      <thead>
+        <tr>
+          <th>POS</th>
+          <th>NAME</th>
+          <th>PJ</th>
+          <th>PTS</th>
+          <th>DG</th>
+        </tr>
+      </thead>
+      <tbody>
+  `;
+
+	standings.forEach((teamData) => {
+
+		const rank=teamData.rank;
+		const logo = teamData.team.logo
+		const teamName = teamData.team.name;
+		const played = teamData.all.played;
+		const points = teamData.points;
+		const goalDiff = teamData.goalsDiff;
 
 
+		container.querySelector(".scrollable-table table tbody").innerHTML += `
+			<tr>
+				<td>${rank}</td>
+				<td class="team-name"> <img class="team-logo" src="${logo}" alt="team-logo"> ${teamName}</td>
+				<td>${played}</td>
+				<td>${points}</td>
+				<td>${goalDiff}</td>
+			</tr>
+		`
+  });
 
-
-
-
+	container.innerHTML += `
+      </tbody>
+    </table>
+	</div>
+  `
+}
